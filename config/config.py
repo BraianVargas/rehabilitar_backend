cd08662ccd7e142d53e832d7dde27300a673e9e2
Braian Vargas <braianvargas1616@gmail.com>
2023-11-06 19:34:10 -0300

    listo para deploy

--- config/config.py
+++ config/config.py
@@ -1,25 +1,26 @@
 ###################################################################################
-#                               PRODUCCION
+#                               Deploy
 ###################################################################################
 # ROOT_PATH = "vctest.dyndns.org:5000"
+ROOT_PATH = "localhost:5000"
 
-# # Base de datos
-# server_host="vctest.dyndns.org"
-# server_user="rehabilitardb"
-# server_passwd='Rehabilitar23!'
-# server_db='rehabilitar'
-# server_port=13306
+# Base de datos
+server_host="127.0.0.1"
+server_user="rehabilitardb"
+server_passwd='Rehabilitar23!'
+server_db='rehabilitar'
+server_port=13306
 
 ###################################################################################
 #                               DEVELOPMENT
 ###################################################################################
 # ROOT_PATH = "vctest.dyndns.org:5002"
-ROOT_PATH = "localhost:5000"
+# ROOT_PATH = "localhost:5000"
 
 
-# Base de datos
-server_host="vctest.dyndns.org"
-server_user="rehabilitardb"
-server_passwd='Rehabilitar23!'
-server_db='rehabilitar_test'
-server_port=13306
+# # Base de datos
+# server_host="vctest.dyndns.org"
+# server_user="rehabilitardb"
+# server_passwd='Rehabilitar23!'
+# server_db='rehabilitar_test'
+# server_port=13306
