e1185576bf5e4eea8ed3dd370cf7b60bb6253f9f
Braian Vargas <braianvargas1616@gmail.com>
2024-01-06 19:16:36 -0300

    [FIX] Get Areas Atendidas

--- modules/estaciones/controller.py
+++ modules/estaciones/controller.py
@@ -3,8 +3,14 @@ from modules.turnos.controller import get_turno_by_id
 
 
 def get_areas_by_tipo_ficha(id_turno):
+    areas_atendidas= []
     turno = get_turno_by_id(id_turno)
+    areas = apiDB.consultaSelect("select * from area")
     query = f"select distinct area_id from fact_tipo_ficha where tipo_ficha_id = '{turno[0]['tipo_ficha_id']}'"
-    areas = apiDB.consultaSelect(query, (turno[0]['tipo_ficha_id']),)
-    
-    return areas
+    fact_areas = apiDB.consultaSelect(query, (turno[0]['tipo_ficha_id']),)
+    for area in areas:
+        for ficha in fact_areas:
+            if area['id'] == ficha['area_id']:
+                areas_atendidas.append(area)
+    return areas_atendidas
+
